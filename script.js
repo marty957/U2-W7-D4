260f2445f699aa45a49281718b845d58aa5f6b25
marty957 <fantuzzimartina@yahoo.it>
2024-11-14 16:58:50 +0100

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const url = "https://api.pexels.com/v1/search?query=ocean";
+const url = "https://api.pexels.com/v1/search?query=";
 const key = "0xhb9QUuuo3YiiLUh9m1MEphFpHGocvBuOa2bRTNj07winZkhhhEu5Zf";
+const btnLoad = document.getElementById("FirstLoad");
+const secondBtnLoad = document.getElementById("secondLoad");
+const query1 = "cat";
+const query2 = "nature";
 
-const getPhotos = function () {
-  const btnLoad = document.getElementById("FirstLoad");
-  const secondBtnLoad = document.getElementById("secondLoad");
+const getPhotos = function (url) {
   fetch(url, {
     method: "GET",
     headers: { Authorization: key }
@@ -16,11 +18,39 @@ const getPhotos = function () {
         throw new Error("Sorry, download failed");
       }
     })
-    .then((arr) => {
-      console.log(arr);
+    .then((photos) => {
+      console.log(photos);
+      const container = document.querySelector(".row");
+      const col = document.createElement("div");
+      col.classList = "col-4";
+      photos.forEach((photo) => {
+        const card = document.createElement("div");
+        card.innerHTML = `<div class="card mb-4 shadow-sm">
+            <img src="https://picsum.photos/id/237/300/200" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem Ipsum</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">
+                    View
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>`;
+      });
     });
 };
+btnLoad.addEventListener("click", () => getPhotos(url + query1));
+secondBtnLoad.addEventListener("click", () => getPhotos(url + query2));
 
-window.addEventListener("DOMContentLoaded", function () {
-  getPhotos();
-});
+//window.addEventListener("DOMContentLoaded", function () {
+// getPhotos();
+//});
